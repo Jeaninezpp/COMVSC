8a5849f8ec59c53dbe846079e529383754794fb4
Jeaninezpp <48111004+Jeaninezpp@users.noreply.github.com>
2021-03-18 16:20:30 +0800

    Update run.m

--- run.m
+++ run.m
@@ -1,53 +1,74 @@
 clc;
 clear;
 addpath ./measure ./tsne
-datasetname = {'WikipediaArticles','bbcsport_seg14of4','Handwritten_numerals',...
-         'MSRCV1','Caltech101-7','Caltech101-20'};
+path = './'; 
+datapath = '../0datasets/';
 
-for di = 1:5
-    path = ['.\dataset\',datasetname{di},'.mat']
-    f=load(path);
-    data=f.X;
-    label=f.Y;
-    viewN = length(data);
-    k = length(unique(label));
+
+datasetname = {'bbcsport_seg14of4','yaleA_3view'};
+for di = 1  % Datasets
+    dataName = datasetname{di};
+    load([datapath,'/',dataName,'.mat'],'X','Y');   % Ubuntu
+    disp(['\n\n Current dataset  : ',dataName]);
     
-    FV = cell(size(data)); 
-    WV= cell(size(data));
-    LV = cell(size(data));
+    viewN = length(X);
+    k = length(unique(Y));
+    N = length(Y);
     
-    for i=1:viewN % Normalization
-        data{i} = mapstd(data{i}',0,1);
+    for iv = 1:viewN
+        X{iv} = mapstd(X{iv}',0,1); 
+        data{iv} = X{iv}';
     end
-
+    % X{i} is d * N
+    % data{i} is N *d
+    
+    FV = cell(size(X));
+    WV= cell(size(X));
+    LV = cell(size(X));
     for i =1:viewN
-        W = constructW_PKN(data{i}, 5, 1);  %   X : d*n  / 5 neighbors /  is symmetric
+        W = constructW_PKN(X{i}, 5, 1);  %   X : d*n  / 5 neighbors /  is symmetric
         D = diag(sum(W));
         L = D-W;
-        [Fv, ~, ~]=eig1(L,k,0);
+        [Fv, ~, ~]=eig1(L,k,0); 
         FV{i} = Fv;
         LV{i} = L;
         WV{i} = W;
     end
 
-    lmd = 10.^[1:1:5];
-    gma = [1:0.1:2];
 
+    lmd = 2.^[3:2:13];
+    gma = [1.3:0.2:2.7];
+    para3=1;
+    para4=1; % Fixed
 
-    B=cell(size(data)); % B=inv(X'X+alpha*I)^(-1)
-    %========for========
+    
+    B=cell(size(X)); % B=inv(X'X+alpha*I)^(-1)
+    idx = 1; 
     for i=1:length(lmd)
-        for vnum=1:viewN % view
-            B{vnum}= inv(data{vnum}'*data{vnum}+lmd(i)*eye(size(data{1},2)));
+        for vnum=1:viewN 
+            % B{vnum}= inv(X{vnum}'*X{vnum}+lmd(i)*eye(size(X{1},2)));
+            I_d{vnum} = eye(size(X{vnum},1)); % X : d*N
+            I_n{vnum} = eye(size(X{vnum},2));
+            B{vnum} = (1/lmd(i))*(I_n{vnum}-(1/lmd(i))*X{vnum}'*inv(I_d{vnum}+(1/lmd(i))*X{vnum}*X{vnum}')*X{vnum});
         end
         for j=1:length(gma)
-           disp(['alpha:',num2str(lmd(i)),' gamma:',num2str(gma(j))]);
-           for it = 1:1
-               tic;
-               [res,Fv,Fstar,Lv,Wv,Yres,obj]=CLOMV(data,label,B,lmd(i),gma(j),para3(p),para4(q),FV,WV,LV);
-               t=toc;
-               disp(['ACC: ',num2str(res(end,7)), '  Time: ',num2str(t)]);
-           end
+            tic;
+            [ress,Fv,Fstar,Lv,Wv,Yres,obj]=CLOMV_w(X,Y,B,lmd(i),gma(j),para3,para4,FV,WV,LV);
+            t(idx)=toc;
+            res_comvsc = ress(end,:);
+            Result_11COMVSC(idx,:) = [lmd(i) gma(j) res_comvsc];
+            disp(['Para1 = ',num2str(lmd(i)),'  Para2 = ',num2str(gma(j)), '  ACC = ', num2str(res_comvsc(:,7)),'  Time = ',num2str(t(idx))]);
+            ourFscore(i,j)  = res_comvsc(1); ourPrecision(i,j) = res_comvsc(2); ourRecall(i,j) = res_comvsc(3); 
+            ourNmi(i,j) = res_comvsc(4);ourAR(i,j) = res_comvsc(5); ourEntropy(i,j) = res_comvsc(6); 
+            ourACC(i,j) = res_comvsc(7); ourPurity(i,j) = res_comvsc(8);
+%             dlmwrite(['./', dataName, '.txt'], Result_11COMVSC(idx,:),'-append','delimiter','\t','newline','pc');
+            idx = idx+1;
         end
     end
+    maxresult = max(Result_11COMVSC,[],1);
+    Allresult(1,:) = maxresult(3:10);
+    fprintf('- Finish Proposed Method \n ');
+
+    save(['./' , dataName ,'_Our.mat'], 't','Allresult');
+
 end
